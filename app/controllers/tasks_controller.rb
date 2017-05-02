7638e3c69907621ae9a5ab9eedd942237a609229
Diljith Dinesh <mddiljith@gmail.com>
2017-05-03 00:53:00 +0530

    Heroku

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,91 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  # GET /tasks
+  # GET /tasks.json
+  def index
+    #@tasks = Task.all
+    json_resp = {}
+    date_str = "06 Apr 2016"
+    str_date = date_str.to_datetime
+    current_date = Date.today
+    sixth_date = str_date + 430.days
+    seventh_date = str_date + 530.days
+    json_resp = {
+      buy_date: stringFormat(str_date),
+      next_service: stringFormat(sixth_date),
+      future_service: stringFormat(seventh_date)
+    }
+
+    render json: json_resp
+  end
+
+  # GET /tasks/1
+  # GET /tasks/1.json
+  def show
+  end
+
+  # GET /tasks/new
+  def new
+    @task = Task.new
+  end
+
+  # GET /tasks/1/edit
+  def edit
+  end
+
+  # POST /tasks
+  # POST /tasks.json
+  def create
+    @task = Task.new(task_params)
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to @task, notice: 'Task was successfully created.' }
+        format.json { render :show, status: :created, location: @task }
+      else
+        format.html { render :new }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /tasks/1
+  # PATCH/PUT /tasks/1.json
+  def update
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
+        format.json { render :show, status: :ok, location: @task }
+      else
+        format.html { render :edit }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /tasks/1
+  # DELETE /tasks/1.json
+  def destroy
+    @task.destroy
+    respond_to do |format|
+      format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def task_params
+      params.require(:task).permit(:title, :completed, :order)
+    end
+
+    def stringFormat date
+      return date.strftime('%d/%b/%Y')
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170502183250) do
+
+  create_table "tasks", force: true do |t|
+    t.string   "title"
+    t.boolean  "completed"
+    t.integer  "order"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
